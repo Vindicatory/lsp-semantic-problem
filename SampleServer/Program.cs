5d3df30cfa38e48d25138e2ac377fde891c36a10
Vindicator <43339065+Vindicatory@users.noreply.github.com>
2025-01-21 17:47:50 +0300

    server

---
+++ SampleServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using OmniSharp.Extensions.LanguageServer.Server;
+using SampleServer;
+
+public abstract class Program
+{
+    public static async Task Main()
+    {
+        var server = await LanguageServer.From(
+            opts =>
+                opts
+                    .WithInput(Console.OpenStandardInput())
+                    .WithOutput(Console.OpenStandardOutput())
+                    .WithHandler<SemanticTokensHandler>()
+        );
+        
+        await server.WaitForExit;
+    }
+}
---
+++ SampleServer/SemanticTokensHandler.cs
@@ -0,0 +1,39 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
+
+
+namespace SampleServer;
+
+public class SemanticTokensHandler : SemanticTokensHandlerBase
+{
+   protected override SemanticTokensRegistrationOptions CreateRegistrationOptions(SemanticTokensCapability capability,
+      ClientCapabilities clientCapabilities)
+   {
+      return new SemanticTokensRegistrationOptions
+      {
+         DocumentSelector = TextDocumentSelector.ForLanguage("testfiletype"),
+         Legend = new SemanticTokensLegend
+         {
+            TokenTypes = Container.From(new []{new SemanticTokenType("TestToken")}),
+            TokenModifiers = capability.TokenModifiers
+         },
+         Full = true
+      };
+   }
+
+   protected override Task Tokenize(SemanticTokensBuilder builder, ITextDocumentIdentifierParams identifier,
+      CancellationToken cancellationToken)
+   {
+      var range = new Range(1, 1, 1, 10);
+      builder.Push(range, "TestToken");
+
+      return Task.CompletedTask;
+   }
+
+   protected override Task<SemanticTokensDocument> GetSemanticTokensDocument(ITextDocumentIdentifierParams @params, CancellationToken cancellationToken)
+   {
+      return Task.FromResult(new SemanticTokensDocument(RegistrationOptions));
+   }
+}
